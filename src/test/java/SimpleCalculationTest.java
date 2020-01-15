5c4584e67c490e9b02b389ef2d41a63f440c5c0f
LincmG <lincolnmgijima@gmail.com>
2020-01-15 11:28:27 +0200

    fixedtesting

--- src/test/java/SimpleCalculationTest.java
+++ src/test/java/SimpleCalculationTest.java
@@ -5,22 +5,17 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 public class SimpleCalculationTest {
 
     @Test
-    void testAddition(){
-        assertEquals(20, SimpleCalculation.add(4,2,6,8));
-    }
-
-    @Test
-    void testAddition2(){
-        assertEquals(9, SimpleCalculation.add(3,6));
+    void add(){
+        assertEquals(15,SimpleCalculation.add(1,2,3,4,5));
+        assertEquals(3,SimpleCalculation.add(1,2));
+        assertEquals(-2,SimpleCalculation.add(-1,-1));
     }
     @Test
-    void testMulti(){
-        assertEquals(11412, SimpleCalculation.multiply(1902,6));
+    void multiply(){
+        assertEquals(3,SimpleCalculation.multiply(1,3));
+        assertEquals(-3,SimpleCalculation.multiply(-1,3));
+        assertEquals(120,SimpleCalculation.multiply(1,2,3,4,5));
+        assertEquals(3,SimpleCalculation.multiply(1,3));
+        assertEquals(-3,SimpleCalculation.multiply(-1,3));
     }
-
-    @Test
-    void testMulti2(){
-        assertEquals(36, SimpleCalculation.multiply(2,2,3,3));
-    }
-
 }
